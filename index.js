080af079ce58ac1dd6c1ee6b23fd4cc8d51b4e0d
unknown <srinikumaran1995@gmail.com>
2022-03-30 11:24:52 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const $addTodo = document.getElementById("addTodo");
+const $todoList = document.getElementById("todoList");
+
+$addTodo.addEventListener("keypress", (e) => {
+    if (e.code === "Enter" && e.target.value !== "") {
+        let todo = e.target.value;
+        let $todoItem = document.createElement("li");
+
+        let todoId = new Date().getTime();
+
+        $todoItem.innerHTML = `
+            <input id="${todoId}" type="checkbox"/>
+             <label for="${todoId}">${todo}</label>
+        `;
+        $todoList.appendChild($todoItem);
+        $addTodo.value = "";
+    }
+});
+$todoList.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        e.target.parentElement.classList.add('complete');
+    } else {
+        e.target.parentElement.classList.remove('complete');
+    }
+    console.log(e.target);
+
+});
